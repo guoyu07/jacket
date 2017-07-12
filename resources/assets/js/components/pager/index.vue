2ac1a3223766a293f13f15dabd646edc46e68ead
caohui <772071248@qq.com>
2017-07-12 23:13:21 +0800

    初始提交

---
+++ resources/assets/js/components/pager/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <h1>This is the first page</h1>
+        </div>
+    </div>
+
+</template>
+<script>
+    export default{
+        name:'index'
+    }
+</script>
